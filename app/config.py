94e5c4ffe121e0d1b26505f3f7859899efce2410
Murad <seferov.murad.98@gmail.com>
2025-06-26 17:13:41 +0400

    add_config_file

---
+++ app/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+
+    @property 
+    def DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}/{self.POSTGRES_DB}"
+    
+    model_config = SettingsConfigDict(env_file='.env')
+
+setting = Settings()
+

db103d692c6508ecd53d0f9dc940b97aec061b60
Murad <seferov.murad.98@gmail.com>
2025-06-27 15:15:25 +0400

    'create_Task_model'

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app.database import Base, int_pk
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean
+
+
+
+class Task(Base):
+    id: Mapped[int_pk]
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    is_done: Mapped[bool] = mapped_column(Boolean, default=False)

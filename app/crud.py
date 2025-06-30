555a8bd8a36ac14b51c29d5c4662abd220bbe11a
Murad <seferov.murad.98@gmail.com>
2025-06-30 17:08:20 +0400

    'Get_method_task'

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,29 @@
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from app.schemas import TaskIn
 from app.models import Task
+from typing import List
 
-async def create_task(task_in: TaskIn, db: AsyncSession) -> Task:
+async def create_task_service(task_in: TaskIn, db: AsyncSession) -> Task:
     new_task = Task(**task_in.dict())
     db.add(new_task)
     await db.commit()
     await db.refresh(new_task)
     return new_task
+
+
+async def get_all_tasks_service(db: AsyncSession) -> List[Task]:
+    stmt  = select(Task)
+    result = await db.execute(stmt)
+    tasks = result.scalars().all()
+    return tasks
+
+async def get_task_service(task_id: int, db: AsyncSession) -> Task:
+    stmt = select(Task).filter(Task.id == task_id)
+    result = await db.execute(stmt)
+    task = result.scalars().first()
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Task not found!')
+    return task
+    
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Depends
-from app.crud import create_task
+from app.crud import create_task_service, get_all_tasks_service, get_task_service
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_session
 from app.schemas import TaskIn, TaskOut
+from typing import List
 
 router = APIRouter(prefix='/tasks', tags=['Tasks'])
 
 @router.post('/create', response_model=TaskOut)
-async def create(task_in: TaskIn, db: AsyncSession = Depends(get_session)):
-    return await create_task(task_in, db)
+async def create_task(task_in: TaskIn, db: AsyncSession = Depends(get_session)):
+    return await create_task_service(task_in, db)
+
+@router.get('/all', response_model=List[TaskOut])
+async def get_all_task(db: AsyncSession = Depends(get_session)):
+    return await get_all_tasks_service(db)
+
+@router.get('/task/{task_id}', response_model=TaskOut)
+async def get_task(task_id: int, db: AsyncSession = Depends(get_session)):
+    return await get_task_service(task_id, db)
